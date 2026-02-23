1e09b2b27002f3debe268da893ae4ecf8e266148
Your Name <you@example.com>
2026-02-23 23:14:38 +0530

    Fix root URL redirect

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from projects_app.views import projects_list
+
 urlpatterns = [
+    path('', projects_list, name='home'),
     path('admin/', admin.site.urls),
     path('projects/', include('projects_app.urls')),
     path('api/', include('api_app.urls')),
 ]
+
