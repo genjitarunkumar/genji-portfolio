f1964b553ae1cddd7c38f27667d86e8bd25ba16c
Your Name <you@example.com>
2026-03-15 04:50:34 +0530

    Run Django migrations during Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
+python3.9 manage.py collectstatic --noinput
 echo "BUILD END"
